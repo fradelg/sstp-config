b0a849d7c8dd573590c7d1dd02cd2e4b2a272d79
Fco. Javier Delgado del Hoyo <frandelhoyo@gmail.com>
2016-05-20 11:36:38 +0200

    added install notes and output in script

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # sstp-config
+
 Shell script to configure a new SSTP connection in Linux
+
+# Installation
+
+```
+git clone https://github.com/fradelg/sstp-config.git
+cd sstp-config.git
+chmod u+x sstp-config.sh
+```
 
 ## Example 
 
--- sstp-config.sh
+++ sstp-config.sh
@@ -5,20 +5,24 @@
 # $4 server
 # $5 path_to_cert
 
+echo "Updating certification authorities ..."
 sudo cp $5 /usr/local/share/ca-certificates/$1.pem
 sudo update-ca-certificates
 
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' sstp-client | grep "install ok installed")
-echo "Checking for sstp-client: $PKG_OK"
+echo "Checking for sstp-client ... $PKG_OK"
 if [ "" == "$PKG_OK" ]; then
-  echo "Installing sstp-client."
+  echo "Installing sstp-client using PPA ..."
+  sudo apt-get --yes install software-properties-common
   sudo add-apt-repository ppa:eivnaes/network-manager-sstp
   sudo apt-get update
-  sudo apt-get --force-yes --yes install sstp-client
+  sudo apt-get --yes install sstp-client
 fi
 
+echo "Adding user credential to chap-secrets ..."
 echo "$2 $1 '$3' *" | sudo tee -a /etc/ppp/chap-secrets
 
+echo "Adding new connection config to PPP peers ..."
 echo "remotename      $1
 linkname        $1
 ipparam         $1
@@ -33,6 +37,7 @@ defaultroute
 debug
 file /etc/ppp/options.pptp" | sudo tee /etc/ppp/peers/$1
 
+echo "Updating ip-up scripts to add route when connecting ..."
 echo "!/bin/bash
 NET=\`echo \$4 | cut -d . -f 1,2,3\`
 route add -net \$NET.0/24 dev \$1" | sudo tee /etc/ppp/ip-up.d/0route
